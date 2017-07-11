4c9e2dd6bbe924ca0a0549c5f3d3c5643dcac5a2
Omer Coding <omerh127@gmail.com>
2017-07-12 01:32:15 +0300

    added rainbow-mode and delete-mode

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -1,5 +1,7 @@
+var click = false;
+var grid = 16;
+
 $(document).ready(function(){
-  var grid = 15;
   for(var i=0; i<grid; i++){
     for(var j=0; j<grid; j++){
       $('.grid').append("<div class='box'></div>");
@@ -16,21 +18,18 @@ $(document).ready(function(){
   });
 });
 
-$(document).on("click", ".but", function(){
-  $('.boxed').addClass("box");
-  $('.boxed').removeClass("boxed");
-  var new_grid = 15;
-  new_grid = prompt("Please enter the new grid size", "between 1-64");
-  while (new_grid > 64 || new_grid < 1){
-    new_grid = prompt("Wrong. Please enter the new grid size", "between 1-64");
+$(document).on("click", "#db", function(){
+  grid = prompt("Please enter the new grid size", "between 1-64");
+  while (grid > 64 || grid < 1){
+    grid = prompt("Wrong. Please enter the new grid size", "between 1-64");
   };
   $('.grid').empty();
-  for(var i=0; i<new_grid; i++){
-    for(var j=0; j<new_grid; j++){
+  for(var i=0; i<grid; i++){
+    for(var j=0; j<grid; j++){
       $('.grid').append("<div class='box'></div>");
     }
   }
-  var change = "calc(100% * (1/" + new_grid + ") - 2px)"
+  var change = "calc(100% * (1/" + grid + ") - 2px)"
   $('.box').css({"width" : change, "height" : change});
   $('.boxed').css({"width" : change, "height" : change});
   $('.box').mouseenter(function() {
@@ -38,7 +37,35 @@ $(document).on("click", ".but", function(){
     $(this).removeClass("box");
     var num = (document.querySelectorAll('.box').length);
     if (num < 1){
-      $('.grid').css("border" , "3px solid green");
+      $('.boxed').css({"background-color" : "green", "border" : "1px solid green"});
+    };
+  });
+});
+
+$(document).on("click", "#rb", function() {
+  $('.box, .boxed').mouseenter(function() {
+    var colorR = Math.floor((Math.random() * 256));
+    var colorG = Math.floor((Math.random() * 256));
+    var colorB = Math.floor((Math.random() * 256));
+    $(this).css("background-color", "rgb(" + colorR + "," + colorG + "," + colorB + ")");
+  });
+});
+
+$(document).on("click", "#bm", function() {
+  $('.grid').empty();
+  for(var i=0; i<grid; i++){
+    for(var j=0; j<grid; j++){
+      $('.grid').append("<div class='box'></div>");
+    }
+  }
+  var change = "calc(100% * (1/" + grid + ") - 2px)"
+  $('.box').css({"width" : change, "height" : change});
+  $('.boxed').css({"width" : change, "height" : change});
+  $('.box').mouseenter(function() {
+    $(this).addClass("boxed");
+    $(this).removeClass("box");
+    var num = (document.querySelectorAll('.box').length);
+    if (num < 1){
       $('.boxed').css({"background-color" : "green", "border" : "1px solid green"});
     };
   });
